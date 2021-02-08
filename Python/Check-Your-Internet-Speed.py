36d520a5a17372d8ec5eb873f7769b43f4ef5689
Sumit Gole <67162604+sumit-gole@users.noreply.github.com>
2021-02-08 21:39:03 +0530

    Update Check-Your-Internet-Speed.py

--- Python/Check-Your-Internet-Speed.py
+++ Python/Check-Your-Internet-Speed.py
@@ -15,3 +15,28 @@ elif option == 3:
     print(st.results.ping, 'b/s')
 else:
     print("Please Enter the Correct Choice :  ")
+
+  """ ________________________________________________________________________________________________________________________________________________________
+  runfile('C:/Users/Sumit/Desktop/Python Bootcamp/Python/Check Your Internet Speed.py', wdir='C:/Users/Sumit/Desktop/Python Bootcamp/Python')
+
+ What Speed Do You Wan't to Test : 
+ 1) Download Speed
+ 2) Upload Speed
+ 3) Ping                   
+ Your Choice :  1
+629876.2959293812 b/s
+
+ What Speed Do You Wan't to Test : 
+ 1) Download Speed
+ 2) Upload Speed
+ 3) Ping                   
+ Your Choice :  2
+5890753.938074739 b/s
+
+ What Speed Do You Wan't to Test : 
+ 1) Download Speed
+ 2) Upload Speed
+ 3) Ping                   
+ Your Choice :  3
+0 b/s
+"""
